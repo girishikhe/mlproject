5afa31f4a7472505d7288206ad97ca7143c07745
Girish <girish.ikhe.vu.com>
2024-04-27 16:44:40 +0530

    transformation creatinng

---
+++ src/mlProject/components/data_transformation.py
@@ -0,0 +1,120 @@
+import sys
+import os
+from pathlib import Path
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+from src.mlProject.logger.logging import logging
+from src.mlProject.exception.exception import customexception
+
+
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+
+from src.mlProject.utils.utils import save_object
+
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_file_object = os.path.join("artifacts", "preprocessor.pkl")
+    
+class DataTranformation:
+    def __init__(self):
+        self.data_transformation_confg = DataTransformationConfig
+        
+    def get_data_transformation(self):
+        try:
+            logging.info("Data transformation is started")
+            cat_cols = ['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch','test_preparation_course']
+            num_cols = ['reading_score', 'writing_score']
+            
+            num_pipeline =Pipeline(
+                steps=[
+                ("imputer", SimpleImputer(strategy="median")),
+                ("scalar", StandardScaler())
+                       
+                       ]
+                
+            )
+            
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scalar", StandardScaler())
+                ]
+            )
+            
+            logging.info(f"Categorical columns: {cat_cols}")
+            logging.info(f"Numerical columns: {num_cols}")
+            
+            
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, num_cols),
+                    ("col_pipeline", cat_pipeline,  cat_cols)
+                ]
+            )
+                       
+            return preprocessor
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+            raise customexception(e, sys)
+    
+    
+    def initialize_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            logging.info("read the train and test data is completed")
+            logging.info(f'Train Dataframe Head : \n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head : \n{test_df.head().to_string()}')
+            
+            preprocessing_obj= self.get_data_transformation()
+            
+            
+            target_column_name = 'math_score'
+            numerical_columns = ["writing_score", "reading_score"]
+            
+            input_feature_train_df=train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df=test_df[target_column_name]
+            
+            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe.")
+            
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            
+            
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+            
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            
+            logging.info("preprocessing pickle file saved")
+            
+            return (
+                train_arr,
+                test_arr
+            )
+            
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+
+            raise customexception(e,sys)
+        
+
+    
--- src/mlProject/utils/utils.py
+++ src/mlProject/utils/utils.py
@@ -3,8 +3,8 @@ import sys
 import pickle
 import numpy as np
 import pandas as pd
-from src.logger.logging import logging
-from src.exception.exception import customexception
+from src.mlProject.logger.logging import logging
+from src.mlProject.exception.exception import customexception
 
 from sklearn.metrics import r2_score, mean_absolute_error,mean_squared_error
 
